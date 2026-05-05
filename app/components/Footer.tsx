29a4df4f2a529738491ae98ee65b9963c79a4c09
TeqbM <mukesh@teqbuddies.com>
2026-05-05 15:38:15 +0530

    footer changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
      return(
-          <footer className="fixed bottom-3 px-3 left-3 right-3 py-3 bg-amber-600 text-center rounded-lg">
+          <footer className="absolute bottom-3 px-3 left-3 right-3 py-3 bg-amber-600 text-center rounded-lg">
                <div className="container mx-auto text-white">
                Copyright © Your Website {new Date().getFullYear()}
                </div>
